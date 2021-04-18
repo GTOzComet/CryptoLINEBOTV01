e5f9d58b7b7b319772988a9796075b83c8673dcd
GTOzComet <76593165+GTOzComet@users.noreply.github.com>
2021-04-18 20:15:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ line_bot_api = LineBotApi(lineaccesstoken)
 ####################### new ########################
 @app.route('/')
 def index():
-    return "Hello World!12345"
+    return "Hello World!123455"
 
 
 @app.route('/webhook', methods=['POST'])
